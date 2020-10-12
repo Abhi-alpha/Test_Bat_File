7163b1177f5b79710b187938cf6943a243b6b9ab
guptaa-oup <abhijeetgupta242@gmail.com>
2020-10-12 11:32:02 +0530

    chnages 1

---
+++ Test file.java
@@ -0,0 +1,5 @@
+//made changes in master branch version 3
+
+
+Changes 1 
+My namee is abhijeet 
---
+++ pushinGitHub.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+C:\Project\Dummy Project\Test_batch_file 2\Test_Bat_File
+git status
+git add .
+git commit -am "made changes v1"
+git push
+echo Press Enter...
+read
